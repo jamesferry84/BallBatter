291a8dcb72b8ee097ebe93d568486f85b0f2af95
jamesferry84 <jamesferry84@hotmail.com>
2022-12-13 19:59:10 +0000

    macbook updates

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,38 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Ball : MonoBehaviour
 {
-    [SerializeField] float initialSpeed = 5f;
-    [SerializeField] float maxSpeed = 15f;
+    [SerializeField] float initialSpeed = .05f;
+    [SerializeField] float maxSpeed = .5f;
     Rigidbody2D myRigidBody;
+    [SerializeField] TextMeshProUGUI scoreText;
+    [SerializeField] TextMeshProUGUI livesText;
+    int score = 0;
+    int maxBouncesAtZeroXChange = 2;
+    bool startGame = false;
+
+    int lives = 3;
+    Transform parent;
     // Start is called before the first frame update
     void Start()
     {
-        // Application.targetFrameRate = 120;
+        parent = transform.parent;
+        // Application.targetFrameRate = 120; // sets the framerate;
         myRigidBody = this.GetComponent<Rigidbody2D>();
+        myRigidBody.constraints = RigidbodyConstraints2D.FreezePosition;
+        livesText.text = lives.ToString();
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space) && startGame == false) {
+            startGame = true;
+            myRigidBody.constraints = RigidbodyConstraints2D.None;
+            myRigidBody.AddForce(new Vector2(initialSpeed, initialSpeed));
+            transform.SetParent(null);
+        }
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float moveSpeed = initialSpeed * Time.deltaTime;
-         transform.position += new Vector3(0, moveSpeed, 0);
-        myRigidBody.MovePosition(transform.position);
+
+    }
+
+    void moveBall(Vector2 direction) {
+        // myRigidBody.MovePosition((Vector2)transform.position + (direction * ballSpeed * Time.deltaTime));
     }
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (initialSpeed > 0 && initialSpeed < maxSpeed)
-        {
-            initialSpeed += 1f;
+        if (other.gameObject.tag.Equals("brick")) {
+            score++;
+            Debug.Log(score);
+            scoreText.text = score.ToString();
         }
-        else if (initialSpeed < 0 && initialSpeed > (maxSpeed * -1))
-        {
-            initialSpeed -= 1f;
+    }
+
+    private void EndGame() {
+        //Scenemanager stuff
+        ResetPositions();
+    }
+
+    private void ResetPositions() {
+        startGame = false;
+        myRigidBody.velocity = new Vector2(0,0);
+        transform.SetParent(parent);
+        transform.position = new Vector3(parent.position.x,parent.position.y + 0.25f, 0);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        Debug.Log("entered trigger");
+        if (lives > 0) {
+            lives--;
+            livesText.text = lives.ToString();
+            ResetPositions();
+        } else {
+            EndGame();
         }
-
-        initialSpeed *= -1;
-
+        
     }
 }
--- Assets/Scripts/Brick.cs
+++ Assets/Scripts/Brick.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Brick : MonoBehaviour
 {
+    [SerializeField] int hitPoints = 1;
+    [SerializeField] bool breakable = true;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,6 +21,13 @@ public class Brick : MonoBehaviour
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        Destroy(gameObject);
+        if (!breakable) {
+            return;
+        }
+        hitPoints--;
+        if (hitPoints <= 0) {
+            Destroy(gameObject);
+        }
+        
     }
 }
